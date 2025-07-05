59d754935731e38e68ae54a88f4188a7ff03c176
lasya2624 <lasyachillara24@gmail.com>
2025-07-05 20:43:05 +0530

    Add files via upload

---
+++ To-Do_list/App.jsx
@@ -0,0 +1,41 @@
+// import Renderlist from './Renderlist.jsx'
+//import Img from './img.jsx'
+// import Button from './button.jsx'
+//import Reacthook from './ReactHook.jsx'
+//import Changetheme from './ChangeTheme.jsx'
+// import Change from'./Onchange.jsx'
+// import Colorpicker from'./ColorPicker.jsx'
+// import './index.css'
+//import Spreadoperator from './SpreadOperator.jsx'
+// import Arrayobj from './UpdateArrObject.jsx'
+// import UpdateArr from './UpdateArrObjectsInState.jsx'
+import Todo from './To-Do-List.jsx';
+// function App(){
+//   const fruits=[{name:"apple",nutrients:5},
+//                    {name:"orange",nutrients:8},
+//                    {name:"banana",nutrients:2},
+//                    {name:"mango",nutrients:1}];
+                   
+//   const veggies=[{name:"tomato",nutrients:15},
+//                    {name:"spinach",nutrients:82},
+//                    {name:"cucumber",nutrients:32},
+//                    {name:"carrot",nutrients:63}];                 
+//   return(
+//     <>
+//     {fruits.length > 0 && <Renderlist items={fruits} category="Fruits"/>}
+//     { veggies.length>0 &&  <Renderlist className="veggies" items={veggies} category="Veggies"/>}
+//     </>
+//    //{fruits.length > 0 ? <Renderlist items={fruits} category="Fruits"/>:null}
+//   );
+// }
+
+// function App(){
+//   return(
+//     <Button/>
+//   );
+// }
+
+function App(){
+  return(<Todo/>);
+}
+export default App
